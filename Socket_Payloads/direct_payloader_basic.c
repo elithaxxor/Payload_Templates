6279ad10e90899b9bebff6ad6a627252a1fbc037
adel <86808805+elithaxxor@users.noreply.github.com>
2025-02-24 13:05:45 -0500

    Update direct_payloader_basic.c

--- Socket_Payloads/direct_payloader_basic.c
+++ Socket_Payloads/direct_payloader_basic.c
@@ -1,14 +1,13 @@
 import os
-import socket
+import socket as SOCKET
 import time
 import numpy
 import sys
 import select  ## OS LEEL loop
 import io
-from numpy.lib.format import BUFFER_SIZE
 
-  
-###########################################
+from numpy.lib.format import BUFFER_SIZE
+##########################################
 ##  TODO: MAKE STRUCTURE FOR FILE TRANSFER AND CHAT AND CLIENT/SERVER INFO
 ### TODO: MAKE DATASTRUCTURES A CLASS; SEPERATE FILES BY INDIVIDUAL FUNCTIONS (CWD, CHILD, PARENT, ETC)
 ### TODO: ADD THREADING TO INCLUDE MULTIPLE CLIENTS AND SERVERS
@@ -20,40 +19,43 @@ from numpy.lib.format import BUFFER_SIZE
 
 ## server_ALPHA.py
 ## TODO: 1. Add a class for socket // 2. Add a class for file transfer // 3.  Add a class for chat // 4. Add a class for flask
+## TODO: Add Conditional for IPV4 and IPV6 //
+
+
+## 1. Create a socket object
 BUFFER_SIZE:int = 8192
 CLIENT_PORT:int = 22223
-
-## 1. Create a socket
-## TODO: Add Conditional for IPV4 and IPV6 // (later  on ore important) 
-CLIENT_HOSTNAME = socket.gethostname()
-CLIENT_IP = socket.gethostbyname(CLIENT_HOSTNAME)
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+CLIENT_HOSTNAME = SOCKET.gethostname()
+CLIENT_IP = SOCKET.gethostbyname(CLIENT_HOSTNAME)
+SOCKET = SOCKET.socket(SOCKET.AF_INET, SOCKET.SOCK_STREAM)
 # socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 ## 1a. Display connection details
 print(f"[SYS]connected to {CLIENT_HOSTNAME} with IP {CLIENT_IP} at port: {CLIENT_PORT}")
-print(f"[SYS] Initiating connection..{type(socket)} + {socket}")
-print(f"[SYS] {socket.getsockname()}")  ## getsockname()
-  
+print(f"[SYS] Initiating connection..{type(SOCKET)} + {SOCKET}")
+print(f"[SYS] {SOCKET.getsockname()}")  ## getsockname()
 
 ## 2. Bind the socket to the IP and PORT
-socket.bind((CLIENT_IP, CLIENT_PORT))
-socket.listen(5)  # sets max que  to 5
-print(f"[SYSTEM] {socket.getsockname()}")
+SOCKET.bind((CLIENT_IP, CLIENT_PORT))
+SOCKET.listen(5)  # sets max que  to 5
+print(f"[SYSTEM] {SOCKET.getsockname()}")
 
-## 3. accepting connection
+## accepting connection
 file_name = input("[SYSTEM] Enter File Name: ")
 file_size = os.path.getsize(file_name)
 
-'''[Connection]... sending a file from a server to a client over a socket connection. '''
-client, addr = socket.accept()
+'''sending a file from a server to a client over a socket connection. '''
+## 1. Accept connection,
+client, addr = SOCKET.accept()
 print(f"[+] connected to {client}, {addr}")
 time.sleep(1)
-
 bytes_name = client.send(file_name.encode())
 time.sleep(.5)
 bytes_sent = client.send(str(file_size).encode())
+bytes_left = bytes_name + bytes_sent
+time.sleep(.5)
 
+''' send file name and size to client '''
 print("f[!] File Name and Size sent to client..", bytes_name, "\n" , bytes_sent)
 send_count = 0
 buffer = file_size
@@ -62,14 +64,16 @@ send_start = time.time()
 ''' open and read as binary for parsing '''
 print(f"[+] {file_name} is {file_size} bytes \n[SYSTEM].. Initiating File Transfer.")
 while buffer != file_size:
-    elapsed_time = time.time - file_size - send_start
+
+# Open the file once before the loop
+    elapsed_time = time.time() - file_size - send_start
     print(f"[+] {file_name} Transfer in progress: {send_count} of {file_size} bytes sent. \n[!] Elapsed Time: {elapsed_time}")
     
     with open(file_name, "rb") as file:
         data = file.read(8192)
-
+        print(f"[+] {file_name} Transfer Complete, {send_count} of {file_size} bytes sent., \n[!] Elapsed Time: {elapsed_time}")
         if not (data):
-            print(f"[SYSTEM] {file_name} Transfer Complete, {send_count} of {file_size} bytes sent.")
+            print(f"[?] {file_name} Transfer Complete, {send_count} of {file_size} bytes sent., \n[!] Elapsed Time: {elapsed_time}")
             break  # <- end of F/T
 
         client.sendall(data)
@@ -77,8 +81,6 @@ while buffer != file_size:
         send_curr = time.time()
 
 send_end = time.time()
-
-
 total_time = send_end - send_start
 print(f"[SYSTEM] {file_name} Transfer Complete in: {total_time}")
-socket.close()  # stop
+SOCKET.close()  # stop
